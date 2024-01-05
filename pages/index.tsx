26566e0be2c52f17c71366971b26215921a44831
ivcained <86070833+ivcained@users.noreply.github.com>
2024-01-06 01:55:24 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
+import AddContact from "../components/createBounty";
+
+const Home: NextPage = () => {
+  const address = useAddress();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.addressContainer}>
+        <div className={styles.addressHeader}>
+          <h1>Create Bounty</h1>
+          <ConnectWallet />
+        </div>
+        {address && (
+          <div className={styles.addressListContainer}>
+            <div className={styles.addressListHeader}>
+              <h3>Let's Go!:</h3>
+              <AddContact />
+            </div>
+            <bounties />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
